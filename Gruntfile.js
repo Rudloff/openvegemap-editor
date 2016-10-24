d819e0c8d74310ef5fe5db9ab1e225aba813d137
Pierre Rudloff <contact@rudloff.pro>
2016-10-24 19:08:09 +0200

    Import from https://github.com/Rudloff/openvegemap/

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+/*jslint node: true */
+module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig(
+        {
+            phpcs: {
+                options: {
+                    standard: 'PSR2',
+                    bin: 'vendor/bin/phpcs'
+                },
+                php: {
+                    src: ['*.php', 'classes/*.php', 'controllers/*.php', 'api/*.php', 'editor/*.php']
+                },
+                tests: {
+                    src: ['tests/']
+                }
+            },
+            jslint: {
+                Gruntfile: {
+                    src: ['Gruntfile.js']
+                }
+            },
+            phpunit: {
+                options: {
+                    bin: 'php -dzend_extension=xdebug.so ./vendor/bin/phpunit',
+                    stopOnError: true,
+                    stopOnFailure: true,
+                    followOutput: true
+                },
+                classes: {
+                    dir: 'tests/'
+                }
+            },
+            jsonlint: {
+                manifests: {
+                    src: ['*.json'],
+                    options: {
+                        format: true
+                    }
+                }
+            },
+            fixpack: {
+                package: {
+                    src: 'package.json'
+                }
+            },
+            phpdocumentor: {
+                doc: {
+                    options: {
+                        directory: 'classes/,controllers/,tests/'
+                    }
+                }
+            }
+        }
+    );
+
+    grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-phpunit');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-jsonlint');
+    grunt.loadNpmTasks('grunt-fixpack');
+    grunt.loadNpmTasks('grunt-phpdocumentor');
+
+    grunt.registerTask('lint', ['jslint', 'fixpack', 'jsonlint', 'phpcs']);
+    grunt.registerTask('test', ['phpunit']);
+    grunt.registerTask('doc', ['phpdocumentor']);
+};
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+use OpenVegeMap\Editor\Controller\MainController;
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/config.php';
+
+$app = new \Slim\App();
+$container = $app->getContainer();
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Smarty(__DIR__.'/templates/');
+    $smartyPlugins = new \Slim\Views\SmartyPlugins($c['router'], $c['request']->getUri());
+    $view->registerPlugin('function', 'path_for', [$smartyPlugins, 'pathFor']);
+    $view->registerPlugin('function', 'base_url', [$smartyPlugins, 'baseUrl']);
+
+    return $view;
+};
+$controller = new MainController($container);
+$app->get('/{type}/{id}', [$controller, 'edit']);
+$app->get('/', [$controller, 'search']);
+$app->post('/{type}/{id}', [$controller, 'submit']);
+$app->run();
